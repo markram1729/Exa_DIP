4e7b07425e713fda32e031d591133e0618748bbb
markram1729 <rishikramjallarapu@gmail.com>
2024-04-27 12:23:47 +0530

    working imgcontainer test cv

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ INC_DIR = $(CUR_DIR)/../yinyang/frontend/Interfaces
 CXX_DIR = $(CUR_DIR)/../yinyang/llvm/build/bin/
 LD_FLAGS = -ljpeg -lpng -Wall
 neo :	img_exa.cpp
-	$(CXX_DIR)/clang++ -I$(INC_DIR)  $^ -o $@ $(LD_FLAGS)
+	$(CXX_DIR)/clang++ -I$(INC_DIR) -DBUDDY_ENABLE_OPENCV `pkg-config --cflags --libs opencv4` $^ -o $@ $(LD_FLAGS)
 c:
 	rm neo 
 
--- img_exa.cpp
+++ img_exa.cpp
@@ -2,6 +2,7 @@
 #include "buddy/DIP/ImageContainer.h"
 #include "buddy/Core/Container.h"
 #include "buddy/DIP/imgcodecs/loadsave.h"
+#include <opencv2/opencv.hpp>
 
 template<typename T>
 void print(const std::string &s ="Out",T x=0)
@@ -21,6 +22,21 @@ int main()
 	}
 	size_t ele = myexa.getSize();
 	print<size_t>("array size",ele);
-		
+	print<int>("channels ",myexa.channels());
+	auto *data = myexa.getData();
+	for(size_t i=0;i<ele;i++)
+	{
+		print<float>("at i= "+std::to_string(i),data[i]);
+	}
+	cv::Mat imo = cv::imread("./Img_dir/TestGrayImage.bmp",cv::IMREAD_GRAYSCALE);
+	//intptr_t sizescv[2]={0};
+	print("mat channels",imo.channels());
+	Img<float,2> myex2(imo,nullptr,true);
+	float *datacv = myex2.getData();
+	for(size_t i=0;i<myex2.getSize();i++)
+	{
+		print<float>("myexa2 at i "+std::to_string(i),datacv[i]);
+	}
+	print<int>("channels",myex2.channels());
 	return 0;
 }
