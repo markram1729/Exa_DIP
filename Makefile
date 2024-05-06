0d54978ec1a1e2c90c137e9edf30b09dce30dfec
markram1729 <rishikramjallarapu@gmail.com>
2024-05-06 10:59:21 +0530

    base examples for normalise in cv

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 
 CUR_DIR = .
 INC_DIR = $(CUR_DIR)/../yinyang/frontend/Interfaces
-CXX_DIR = $(CUR_DIR)/../yinyang/llvm/build/bin/
+BIN_DIR = $(CUR_DIR)/../yinyang/llvm/build/bin/
 LD_FLAGS = -ljpeg -lpng -Wall
 neo :	img_exa.cpp
-	$(CXX_DIR)/clang++ -I$(INC_DIR) -DBUDDY_ENABLE_OPENCV `pkg-config --cflags --libs opencv4` $^ -o $@ $(LD_FLAGS)
+	$(BIN_DIR)/clang++ -I$(INC_DIR) -DBUDDY_ENABLE_OPENCV `pkg-config --cflags --libs opencv4` $^ -o $@ $(LD_FLAGS)
 c:
 	rm neo 
 
--- img_exa.cpp
+++ img_exa.cpp
@@ -28,7 +28,7 @@ int main()
 	{
 		print<float>("at i= "+std::to_string(i),data[i]);
 	}
-	cv::Mat imo = cv::imread("./Img_dir/TestGrayImage.bmp",cv::IMREAD_GRAYSCALE);
+	cv::Mat imo = cv::imread("./Img_dir/TestGrayImage.jpg",cv::IMREAD_GRAYSCALE);
 	//intptr_t sizescv[2]={0};
 	print("mat channels",imo.channels());
 	Img<float,2> myex2(imo,nullptr,true);
@@ -38,5 +38,12 @@ int main()
 		print<float>("myexa2 at i "+std::to_string(i),datacv[i]);
 	}
 	print<int>("channels",myex2.channels());
+	cv::Mat img2cv=cv::imread("./Img_dir/TestGrayImage.jpg",cv::IMREAD_GRAYSCALE);
+	img2cv = img2cv/255.0;
+	std::cout<<"Print Mat \n"<<img2cv<<std::endl;
+	cv::Mat check_img=cv::Mat::zeros(img2cv.rows,img2cv.cols,CV_32F);
+	std::cout<<"dd \n"<<check_img<<std::endl;
+	cv::normalize(img2cv,check_img,0,1,cv::NORM_MINMAX,CV_32F);
+	std::cout<<"dd \n"<<check_img<<std::endl;
 	return 0;
 }
