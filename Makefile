1a61de4867a16ffe161956d3224e4759589b034a
markram1729 <rishikramjallarapu@gmail.com>
2024-05-25 11:07:45 +0530

    epsilon based floating comparsion

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 CUR_DIR = .
-INC_DIR = $(CUR_DIR)/../yinyang/frontend/Interfaces
-BIN_DIR = $(CUR_DIR)/../yinyang/llvm/build/bin/
+INC_DIR = $(CUR_DIR)/../yinyangfrk2/frontend/Interfaces
+BIN_DIR = $(CUR_DIR)/../yinyangfrk2/llvm/build/bin/
 LD_FLAGS = -ljpeg -lpng -Wall
 neo :	img_exa.cpp
 	$(BIN_DIR)/clang++ -g -I$(INC_DIR) -DBUDDY_ENABLE_OPENCV `pkg-config --cflags --libs opencv4` $^ -o $@ $(LD_FLAGS)
--- img_exa.cpp
+++ img_exa.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cfloat>
 #include "buddy/DIP/ImageContainer.h"
 #include "buddy/Core/Container.h"
 #include "buddy/DIP/imgcodecs/loadsave.h"
@@ -10,7 +11,16 @@ void print(const std::string &s ="Out",T x=0)
 	std::cout<<s<<" "<<x<<std::endl;
 }
 
-bool testcvdp(cv::Mat imgcv,Img<float,2> myexa)
+bool compare(float a,float b)
+{
+	if(a==b) return true;
+	float eps = a-b;
+	if(eps<0)eps=-eps;
+	if(eps<FLT_EPSILON) return true;
+
+	return false;
+}
+bool testcvdp(cv::Mat imgcv,Img<float,2> myexa,bool norm)
 {
 	if(myexa.getSizes()[0]!=imgcv.rows|| myexa.getSizes()[1]!=imgcv.cols)
 	{
@@ -23,21 +33,49 @@ bool testcvdp(cv::Mat imgcv,Img<float,2> myexa)
 	{
 		for(size_t j=0;j<myexa.getSizes()[1];j++)
 		{
-			float tmp = cv::saturate_cast<float>(imgcv.at<float>(i,j));
-			float tmp1=data[k];
-			if(tmp1!=tmp)
+			if(!norm)
 			{
-				print<float>("my ss ",data[k]);
-				std::cout<<"crdue "<<data[k]<<std::endl;
-				std::cout<<"hh "<<tmp<<" "<<tmp1<<std::endl;
-				printf("UUNN %f %f",tmp,tmp1);
-				std::cout<<"Not equal at  "<<i<<"-"<<j<<std::endl;
-				std::cout<<"ff "<<(float)imgcv.at<float>(i,j)<<std::endl;
-				std::cout<<"imgcv "<<imgcv.at<float>(i,j)<<std::endl;
-				print<float>("",imgcv.at<uchar>(i,j));
-				return false;
+				float tmp = cv::saturate_cast<float>(imgcv.at<uchar>(i,j));
+				float tmp1=data[k];	
+				std::cout<<"hh "<<tmp<<" "<<tmp1<<" "<<(tmp-tmp1)<<std::endl;
+				if(!compare(tmp,tmp1))
+				{
+					print<float>("my ss ",data[k]);
+					std::cout<<"crdue "<<data[k]<<std::endl;
+					std::cout<<"hh "<<tmp<<" "<<tmp1<<std::endl;
+					printf("UUNN %f %f",tmp,tmp1);
+					std::cout<<"Not equal at  "<<i<<"-"<<j<<std::endl;
+					std::cout<<"ff "<<(float)imgcv.at<float>(i,j)<<std::endl;
+					std::cout<<"imgcv "<<(float)imgcv.at<uchar>(i,j)<<std::endl;
+					print<float>("",imgcv.at<uchar>(i,j));
+					return false;
+				}
+				k++;
 			}
-			k++;
+			else{
+				float tmp = cv::saturate_cast<float>(imgcv.at<float>(i,j));
+				float tmp1=data[k];
+				//tmp=tmp*100000.f;
+				//tmp1=tmp1*100000.f;
+				//std::cout<<"hh "<<tmp<<" "<<tmp1<<" "<<(tmp1-tmp)<<std::endl;
+				float eps = (tmp1-tmp);
+				//if(eps<0)eps=-eps;
+				//std::cout<<"flot diff "<<eps<<" d "<<FLT_EPSILON<<std::endl;
+				if(!compare(tmp,tmp1))
+				{
+					print<float>("my ss ",data[k]);
+					std::cout<<"crdue "<<data[k]<<std::endl;
+					std::cout<<"hh "<<tmp<<" "<<tmp1<<std::endl;
+					printf("UUNN %f %f",tmp,tmp1);
+					std::cout<<"Not equal at  "<<i<<"-"<<j<<std::endl;
+					std::cout<<"ff "<<(float)imgcv.at<float>(i,j)<<std::endl;
+					std::cout<<"imgcv "<<(float)imgcv.at<uchar>(i,j)<<std::endl;
+					print<float>("",imgcv.at<uchar>(i,j));
+					return false;
+				}
+				k++;
+			}
+
 		}
 	}
 	return true;
@@ -66,7 +104,7 @@ int main()
 	//testcvdp(imo,);
 	print("mat channels",imo.channels());
 	std::cout<<"test Mat"<<imo<<std::endl;
-	bool k=testcvdp(imo,myexa);
+	bool k=testcvdp(imo,myexa,false);
 	print<bool>("bool k",k);
 	Img<float,2> myex2(imo,nullptr,true);
 	float *datacv = myex2.getData();
@@ -80,7 +118,7 @@ int main()
 	cv::Mat check_img(img2cv.rows,img2cv.cols,CV_32FC1);
 	img2cv.convertTo(check_img,CV_32FC1,1.f/255);
 	std::cout<<"dd \n"<<check_img<<std::endl;
-	bool s =testcvdp(check_img,myex2);
+	bool s =testcvdp(check_img,myex2,true);
 	std::cout<<"out --> "<<s<<std::endl;
 	return 0;
 }
