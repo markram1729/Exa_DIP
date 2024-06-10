4496629a6d857a96ff0382af0caf24ffb340fc0c
markram1729 <rishikramjallarapu@gmail.com>
2024-06-10 09:50:39 +0530

    3D image check

--- img_exa.cpp
+++ img_exa.cpp
@@ -13,14 +13,15 @@ void print(const std::string &s ="Out",T x=0)
 
 bool compare(float a,float b)
 {
-	if(a==b) return true;
-	float eps = a-b;
-	if(eps<0)eps=-eps;
-	if(eps<FLT_EPSILON) return true;
-
-	return false;
+	//if(a==b) return true;
+	//float eps = std::abs(a-b);
+	//std::cout<<"abds "<<abs(a-b)<<"true "<<eps<<std::endl;
+//	if(eps<0)eps=-eps;
+	return ( std::abs(a-b)<FLT_EPSILON);
 }
-bool testcvdp(cv::Mat imgcv,Img<float,2> myexa,bool norm)
+template <typename T,size_t N>
+//bool testcvdp(cv::Mat imgcv,intptr_t sizes[N],Img<float,2> myexa,bool norm)
+bool testcvdp(cv::Mat imgcv,Img<float,N> myexa,bool norm)
 {
 	if(myexa.getSizes()[0]!=imgcv.rows|| myexa.getSizes()[1]!=imgcv.cols)
 	{
@@ -28,30 +29,52 @@ bool testcvdp(cv::Mat imgcv,Img<float,2> myexa,bool norm)
 	}
 	float *data=myexa.getData();
 	int k=0;
-	//std::cout<<" "<<std::typeid(imgcv)<<std::endl;
-	for(size_t i=0;i<myexa.getSizes()[0];i++)
+	size_t batch=0,channel=0;
+	/*if(N==2)
 	{
-		for(size_t j=0;j<myexa.getSizes()[1];j++)
+		batch=1;
+		channel=1;
+		rows = myexa.getSizes()[]
+	}
+	else
+	{
+		batch=sizes[0];
+		channel=sizes[1];
+	}
+	for(size_t i=0;i<batch;i++)
+	{
+		fOr(size_t j=0;j<channel;j++)
+		*/
+	//std::cout<<" "<<std::typeid(imgcv)<<std::endl;
+/*	for(int i=0;i<myexa.getSizes()[0];i++)
+	{
+		for(int j=0;j<myexa.getSizes()[1];j++)
 		{
 			if(!norm)
 			{
-				float tmp = cv::saturate_cast<float>(imgcv.at<uchar>(i,j));
-				float tmp1=data[k];	
-				std::cout<<"hh "<<tmp<<" "<<tmp1<<" "<<(tmp-tmp1)<<std::endl;
-				if(!compare(tmp,tmp1))
-				{
-					print<float>("my ss ",data[k]);
-					std::cout<<"crdue "<<data[k]<<std::endl;
-					std::cout<<"hh "<<tmp<<" "<<tmp1<<std::endl;
-					printf("UUNN %f %f",tmp,tmp1);
-					std::cout<<"Not equal at  "<<i<<"-"<<j<<std::endl;
-					std::cout<<"ff "<<(float)imgcv.at<float>(i,j)<<std::endl;
-					std::cout<<"imgcv "<<(float)imgcv.at<uchar>(i,j)<<std::endl;
-					print<float>("",imgcv.at<uchar>(i,j));
-					return false;
-				}
-				k++;
-			}
+                          float tmp =
+                              cv::saturate_cast<float>(imgcv.at<uchar>(i, j));
+                          float tmp1 = data[k];
+                          std::cout << "hh " << tmp << " " << tmp1 << " "
+                                    << (tmp - tmp1) << std::endl;
+                          if (!compare(tmp, tmp1)) {
+                            print<float>("my ss ", data[k]);
+                            std::cout << "crdue " << data[k] << std::endl;
+                            std::cout << "hh " << tmp << " " << tmp1
+                                      << std::endl;
+                            printf("UUNN %f %f", tmp, tmp1);
+                            std::cout << "Not equal at  " << i << "-" << j
+                                      << std::endl;
+                            std::cout << "ff " << (float)imgcv.at<float>(i, j)
+                                      << std::endl;
+                            std::cout << "imgcv "
+                                      << (float)imgcv.at<uchar>(i, j)
+                                      << std::endl;
+                            print<float>("", imgcv.at<uchar>(i, j));
+                            return false;
+                          }
+                          k++;
+                        }
 			else{
 				float tmp = cv::saturate_cast<float>(imgcv.at<float>(i,j));
 				float tmp1=data[k];
@@ -78,11 +101,13 @@ bool testcvdp(cv::Mat imgcv,Img<float,2> myexa,bool norm)
 
 		}
 	}
+*/
+		
 	return true;
 }
 int main()
 {
-	Img<float,2> myexa =imread<float,2>("./Img_dir/TestGrayImage.jpg",dip::IMGRD_GRAYSCALE);
+	Img<float,3> myexa =imread<float,3>("./Img_dir/TestGrayImage.jpg",dip::IMGRD_COLOR);
 
 	size_t n=myexa.getRank();
 	print<size_t>("My Rank",n);
@@ -99,26 +124,28 @@ int main()
 	{
 		print<float>("at i= "+std::to_string(i),data[i]);
 	}
-	cv::Mat imo = cv::imread("./Img_dir/TestGrayImage.jpg",cv::IMREAD_GRAYSCALE);
+	cv::Mat imo = cv::imread("/home/regaltos/dev/llvm_dev/buddy_exa/Img_dir/YuTu.jpg",cv::IMREAD_COLOR);
 	//intptr_t sizescv[2]={0};
 	//testcvdp(imo,);
 	print("mat channels",imo.channels());
 	std::cout<<"test Mat"<<imo<<std::endl;
-	bool k=testcvdp(imo,myexa,false);
-	print<bool>("bool k",k);
-	Img<float,2> myex2(imo,nullptr,true);
+	//bool k=testcvdp<float,3>(imo,myexa,false);
+	//print<bool>("bool k",k);
+	intptr_t sizesexai[4]={1,imo.channels(),imo.rows,imo.cols};
+	Img<float,4> myex2(imo,sizesexai,false);
 	float *datacv = myex2.getData();
 	for(size_t i=0;i<myex2.getSize();i++)
 	{
 		print<float>("myexa2 at i "+std::to_string(i),datacv[i]);
 	}
 	print<int>("channels",myex2.channels());
+	print("mat channels",imo.channels());
 	cv::Mat img2cv=cv::imread("./Img_dir/TestGrayImage.jpg",cv::IMREAD_GRAYSCALE);
 	std::cout<<"Print Mat \n"<<img2cv<<std::endl;
 	cv::Mat check_img(img2cv.rows,img2cv.cols,CV_32FC1);
 	img2cv.convertTo(check_img,CV_32FC1,1.f/255);
 	std::cout<<"dd \n"<<check_img<<std::endl;
-	bool s =testcvdp(check_img,myex2,true);
-	std::cout<<"out --> "<<s<<std::endl;
+	//bool s =testcvdp<float,2>(check_img,myex2,true);
+	//std::cout<<"out --> "<<s<<std::endl;
 	return 0;
 }
